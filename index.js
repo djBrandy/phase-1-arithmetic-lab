fb031883145a9d7b2b85be45d8197d4ae25a13ae
djBrandy <dandobrandon0@gmail.com>
2024-03-22 07:50:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+var multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1; 
+
+var mod 
+var num3 = 22;
+var num4 = 9;
+mod = num3 % num4
+
+let max = Math.max(10, 20, 15);
